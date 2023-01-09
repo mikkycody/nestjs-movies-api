d3154eee7fc6939b527014b6f1190e090eb1af07
Michael George <horluwatowbeey@gmail.com>
2023-01-09 13:32:42 +0100

    Chore: update swagger schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto, RegisterUserDto } from './dto';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
   @Post('register')
   async register(@Body() dto: RegisterUserDto) {
     return this.authService.register(dto);
--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
